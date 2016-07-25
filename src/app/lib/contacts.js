acebe99a1e0acbe98045d3e111d5e023294a81bc
l.karpenko <kl2.karpenko@gmail.com>
2016-07-25 12:57:40 +0300

    edit change event

--- src/app/lib/contacts.js
+++ src/app/lib/contacts.js
@@ -1,13 +1,14 @@
-import $ from 'jquery';
 import config from 'envConfig';
 
 import diagnostic from './diagnostic';
 
+let diagnosticPlugin = config.process.isProd() && cordova && cordova.plugins && cordova.plugins.diagnostic;
+
 module.exports = {
 	getContactsStatus() {
-		return new Promise((resolve, reject) => {
+		return new Promise((resolve) => {
 			if(config.process.isProd()) {
-				cordova.plugins.diagnostic.getContactsAuthorizationStatus((status) => {
+				diagnosticPlugin.getContactsAuthorizationStatus((status) => {
 					resolve(status);
 				});
 			} else {
@@ -16,11 +17,28 @@ module.exports = {
 		});
 	},
 
-	STATUSES: config.process.isProd() ? cordova.plugins.diagnostic.permissionStatus : {},
+	STATUSES: config.process.isProd() ? diagnosticPlugin.permissionStatus : {},
 
 	isAvailable() {
 		return this.getContactsStatus().then((status) => {
-			return this.STATUSES.GRANTED ? this.STATUSES.GRANTED === status : true;
+			if (this.STATUSES) {
+				switch(status) {
+					case this.STATUSES.GRANTED:
+						return true;
+					break;
+					case this.STATUSES.NOT_DETERMINED:
+						diagnosticPlugin.requestContactsAuthorization();
+						return true;
+					break;
+					case this.STATUSES.DENIED:
+					case this.STATUSES.DENIED_ALWAYS:
+						return false;
+					break;
+					default:
+						return true;
+					break;
+				}
+			}
 		});
 	},
 
--- src/app/modules/contacts/components/item/Search.jsx
+++ src/app/modules/contacts/components/item/Search.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import config from 'envConfig';
+import Tappable from 'react-tappable';
 
 export default class SearchTop extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			value: ""
+		};
 	}
 
 	render() {
 		return (
-			<input
+			<Tappable
+				component="input"
+				id="searchContacts"
 				type="text"
+				onChange={() =>{
+					console.log('change')
+				}}
+				onInput={() =>{
+					console.log('onInput')
+				}}
+				defaultValue={this.state.value}
 				className="input-custom input-search"
 				placeholder="Enter name or phone number"
 			/>
