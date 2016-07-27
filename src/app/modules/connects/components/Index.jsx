a97b971d92e19b23b59bfb46bf2c326c52afbd43
l.karpenko <kl2.karpenko@gmail.com>
2016-07-27 20:53:15 +0300

    add swipe events

--- src/app/modules/connects/components/Index.jsx
+++ src/app/modules/connects/components/Index.jsx
@@ -4,26 +4,6 @@ import AdaptiveWrapper from 'components/layouts/adaptive/Wrapper.jsx';
 
 var Swipeable = require('react-swipeable')
 
-var SampleComponent = React.createClass({
-	render: function () {
-		return (
-			<Swipeable
-				onSwiping={this.swiping}
-				onSwipingUp={this.swipingUp}
-				onSwipingRight={this.swipingRight}
-				onSwipingDown={this.swipingDown}
-				onSwipingLeft={this.swipingLeft}
-				onSwipedUp={this.swipedUp}
-				onSwipedRight={this.swipedRight}
-				onSwipedDown={this.swipedDown}
-				onSwipedLeft={this.swipedLeft}
-				onSwiped={this.handleSwipeAction}>
-				You can be swipe here!
-			</Swipeable>
-		)
-	}
-})
-
 export default class Index extends Component {
 	constructor(props) {
 		super(props);
