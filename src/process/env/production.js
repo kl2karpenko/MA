8c590ea4363071ff78e139e3a6a20a6805955a05
lkarpenko <l.karpenko@kwebbl.com>
2016-08-04 22:35:06 +0300

    edit lang

--- src/process/env/production.js
+++ src/process/env/production.js
@@ -1,12 +1,12 @@
 import config from "./shared";
 
-function getAddressOfHost(isApp) {
-	let hostName = `https://${config.isIOS() ? "ios" : "android"}.app.qaller.net`;
+function getAddressOfHost() {
+	let hostName = `https://${config.process.isIOS() ? "ios" : "android"}.app.qaller.net`;
 	return hostName + "/";
 }
 
-function getTokenHostName(isApp) {
-	let tokenHostName = `https://${config.isIOS() ? "ios" : "android"}.app.qaller.net`;
+function getTokenHostName() {
+	let tokenHostName = `https://${config.process.isIOS() ? "ios" : "android"}.app.qaller.net`;
 	let tokenUrl = "/token";
 
 	return tokenHostName + tokenUrl;
@@ -14,7 +14,7 @@ function getTokenHostName(isApp) {
 
 module.exports = $.extend(true, config, {
 	schema: {
-		hostname: getAddressOfHost(config.process.isBuildApp()),
-		tokenHostname: getTokenHostName(config.process.isBuildApp())
+		hostname: getAddressOfHost(),
+		tokenHostname: getTokenHostName()
 	}
 });
